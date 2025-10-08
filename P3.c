a2cb38bd7f69a4d24f76654d15ca7ed251e14090
siguarinc <siguarinc@unal.edu.co>
2025-10-08 12:18:16 -0500

    szs

--- P3.c
+++ P3.c
@@ -11,7 +11,35 @@ int main(int argc, char *args[]){
     perror("Ya esta creado el semaforo\n");
   } 
   sem_wait(sem1);
-    
+  //aca aun esta bloqueado P3 toca ver en que momento se desbloquea
+  
+  
+  
+  //creacion de memoria compartida:
+  //buscar manera para que el tamaño sea el parametro N*2 convertido en bytes
+  const char NOMBRE = "/MEMP3"
+  const int SIZE = 4096;
+  int fd;
+  void *ptr;
+
+  shm_unlink(NOMBRE);
+
+  int fd = shm_open(NOMBRE, O_CREATE | O_RDWR, 0666);
+  if (fd < 0) {
+    perror("Error en shm_open");
+    return(-1);
+  }
+
+  if(ftruncate(fd, SIZE)<0){
+    perror("Error en ftruncate()\n")
+    return (-2);
+  }
+
+  ptr = mmap(NULL, SIZE, PROT_READ, MAP_SHARED, fd, 0);
+  if (ptr == MAP_FAILED) {
+      perror("Error MAP_FAILED");
+      return (-3);
+  }
                                  
   
 }
--- p1_p2.c
+++ p1_p2.c
@@ -21,9 +21,39 @@ int main(int argc, char *args[]){
   if((int sem_getvalue(sem1)<=-1) && (int sem_getvalue(sem2)<=-1 )){
     pid_t P2=fork();
     if(P2>0){
-    
+
+      int a = args[2];
+      int b = args[3];
+      // Aca se crea el bufer y se agrega a y b
+      for(i=0,i<args[1],i++){
+        b = a + b;
+        a = b;
+
+      const char NOMBRE = "/MEMP3"
+      int fd = shm_open(NOMBRE, O_RDWR, 0666);
+      if (fd < 0) {
+        perror("Error en shm_open"); 
+        return(-1);
+      }
+
+      void *ptr = mmap(NULL, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+      if (ptr == MAP_FAILED) {
+          perror("Error MAP_FAILED");
+          return (-3);
+      }
+      memcpy(ptr,b,sizeof(int))
+         
+        // se va a agregar el nuevo valor de B
+      }
+        
     }else if(P2==0){
-    
+
+      c = args[4];
+      for(i= arg[4],i<args[1],i++){
+        int potencia_de_dos = 2**i;
+        //aca se agrega el dato al buffer 2
+      }
+
     }else{
       perror("Fallo al crear P1\n");
     }
