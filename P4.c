a5ebb3d8cc516ee629f33407536530fdf55b431c
siguarinc <160177840+siguarinc@users.noreply.github.com>
2025-10-08 10:22:06 -0500

    Update P4.c

--- P4.c
+++ P4.c
@@ -3,7 +3,11 @@
 #include <unistd.h>
 #include <pthread.h>
 int main(int argc, char *args[]){0
-
+  sem_t *sem1
+  sem1 = sem_open(/sem1,O_CREAT || O_EXCL,0666,0)
+  if (sem1=="SEM_FAILED"){
+    perror("Ya esta creado el semaforo\n");
+  } 
 
                                  
   
