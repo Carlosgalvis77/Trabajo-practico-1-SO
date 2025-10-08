6d3011fa8168d5ee7f109d32770ca5d5caa51147
siguarinc <160177840+siguarinc@users.noreply.github.com>
2025-10-08 09:43:35 -0500

    Update p1_p2.c

--- p1_p2.c
+++ p1_p2.c
@@ -2,6 +2,16 @@
 # include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
-int main(int argc, char *args[]){
+int main(int argc, char *args[]){0
+
+  P1=fork();
+  if(P1>0){
+    
+  }else if(P1==0){
+    
+  }else{
+    perror("Fallo al crear P1\n");
+  }
+                                 
   
 }
